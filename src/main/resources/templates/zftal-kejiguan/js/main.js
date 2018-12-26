d90e4144dbcdf78eb577fc27f305e8697a24fe47
magi <daodaobendan@gmail.com>
2018-12-26 09:31:22 +0800

    fixed button bug

--- src/main/resources/templates/zftal-kejiguan/js/main.js
+++ src/main/resources/templates/zftal-kejiguan/js/main.js
@@ -5,7 +5,7 @@ function GetUrlParam(name) {
 　　　　if (arrObj.length > 1) {
 　　　　　　var arrPara = arrObj[1].split("&");
 　　　　　　var arr;
-
+           
 　　　　　　for (var i = 0; i < arrPara.length; i++) {
 　　　　　　　　arr = arrPara[i].split("=");
 　　　　　　　　if (arr != null && arr[0] == name) {
